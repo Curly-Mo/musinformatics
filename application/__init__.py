5bc36e6a7abf7aa742a1783725410c7e0196a2e0
Colin Fahy <cfahy@compete.com>
2015-08-30 14:00:52 -0400

    Forgot the application dir (－‸ლ)

---
+++ application/__init__.py
@@ -0,0 +1,24 @@
+"""
+Initialize Flask app
+
+"""
+from flask import Flask
+
+app = Flask('application')
+
+app.config.from_object('application.settings.Production')
+
+# Enable jinja2 loop controls extension
+app.jinja_env.add_extension('jinja2.ext.loopcontrols')
+
+# Pull in URL dispatch routes
+import urls
+
+
+def main():
+    urls.load_urls()
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
+
+if __name__ == "__main__":
+    main()
