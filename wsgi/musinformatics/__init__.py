02029e45a11bb195aba1fdf2c1c4565516aa29a9
Colin Fahy <cfahy@compete.com>
2015-08-31 17:59:18 -0400

    One more import attempt

--- wsgi/musinformatics/__init__.py
+++ wsgi/musinformatics/__init__.py
@@ -6,13 +6,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
-#app.config.from_object('settings.Production')
+app.config.from_object('app.settings.Production')
 
 # Enable jinja2 loop controls extension
 app.jinja_env.add_extension('jinja2.ext.loopcontrols')
 
 # Pull in URL dispatch routes
-import urls
+from app import urls
 
 
 def main():
