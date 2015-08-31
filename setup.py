9dcf07947cd9e8a6330a706d1687cde2974bc0b4
Colin Fahy <cfahy@compete.com>
2015-08-31 19:57:14 -0400

    Add WTForms dependency

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup
 
-setup(name='musinformatics',
-      version='1.0',
-      description='Musinformatics Application',
-      author='Colin Fahy',
-      author_email='colin@cfahy.com',
-      url='https://github.com/Curly-Mo/musinformatics',
-      install_requires=['Flask==0.10.1', 'Flask_Cache==0.13.1'],
-      )
+setup(
+    name='musinformatics',
+    version='1.0',
+    description='Musinformatics Application',
+    author='Colin Fahy',
+    author_email='colin@cfahy.com',
+    url='https://github.com/Curly-Mo/musinformatics',
+    install_requires=[
+        'Flask==0.10.1',
+        'Flask_Cache==0.13.1',
+        'WTForms==2.0.2',
+    ],
+)
