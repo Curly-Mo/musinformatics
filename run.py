712f8e5e5201bd3c98f480603632ee190d8145bb
Colin Fahy <cfahy@compete.com>
2015-09-08 17:19:03 -0400

    Custom librosa fix

--- run.py
+++ run.py
@@ -7,6 +7,13 @@ from musinformatics import generate_keys
 generate_keys.main()
 #app.config.from_pyfile('flaskapp.cfg')
 
+
+import os
+import sys
+
+
+sys.path.insert(1, os.path.join(os.path.abspath('..'), 'libs'))
+
 from musinformatics.app import app
 
 if __name__ == '__main__':
