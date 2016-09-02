43d78fe757026a8b9329709fe0f9b0852e35bd01
Colin Fahy <colin@cfahy.com>
2016-09-02 19:16:36 -0400

    add gevent

--- run.py
+++ run.py
@@ -2,19 +2,20 @@
 Initialize Flask app
 
 """
-
-from musinformatics import generate_keys
-generate_keys.main()
-#app.config.from_pyfile('flaskapp.cfg')
-
-
 import os
 import sys
 
 
 sys.path.insert(1, os.path.join(os.path.abspath('.'), 'libs'))
 
+
 from musinformatics.app import app
+
+
+from musinformatics import generate_keys
+generate_keys.main()
+#app.config.from_pyfile('flaskapp.cfg')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
